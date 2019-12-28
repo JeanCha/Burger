4402ef27ba31360e63ab39656ce51712f277c400
JeanCha <51973809+JeanCha@users.noreply.github.com>
2019-12-28 13:46:13 -0600

    Mysql setup

---
+++ db/seed.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name) VALUES ('Juicy Lucy');
+
+INSERT INTO burgers (burger_name) VALUES ('Cheeseburger Burger');
+
+INSERT INTO burgers (burger_name) VALUES ('Bacon Cheese Burger');
